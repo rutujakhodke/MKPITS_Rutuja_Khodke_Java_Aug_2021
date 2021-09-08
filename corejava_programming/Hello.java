5e4152163e95859c87cef84f03d7794284af790c
rutujakhodke <rutujakho6300@gmail.com>
2021-09-08 15:17:29 +0530

    first java program

---
+++ corejava_programming/Hello.java
@@ -0,0 +1,9 @@
+//2.wap in java to print "hello everybody" and "welcome to mkpits".
+class Hello
+{
+public static void main(String args[])
+{
+System.out.println("Hello Everybody");
+System.out.println("Welcome to MKPITS");
+}
+}
