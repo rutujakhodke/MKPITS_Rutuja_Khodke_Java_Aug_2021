a2b2f14744f05e71d1c08ed2a4273507f2f32213
rutujakhodke <rutujakho6300@gmail.com>
2021-09-24 23:31:52 +0530

    Encapsulation package program

---
+++ Java_Programs/EncapPackage1/Student13.java
@@ -0,0 +1,12 @@
+//4.create a class student has only one field with its setter and getter methods.(create package).
+package com.mk;
+class Student13{
+private String name;
+
+public String getName(){
+ return name;
+}
+public void setName(String name){
+ this.name=name;
+ }
+}
---
+++ Java_Programs/EncapPackage1/TestStudent8.java
@@ -0,0 +1,11 @@
+package com.mk;
+class TestStudent8{
+public static void main(String args[]){
+//creating instance of the encapsulated class.
+Student13 s1=new Student13();
+//setting value in the name member.
+s1.setName("Vijay");
+//getting value of the name member.
+System.out.println(s1.getName());
+ }
+}
