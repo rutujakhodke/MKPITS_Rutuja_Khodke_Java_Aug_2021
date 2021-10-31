65a17591f3baeb9e021ca02f7b6efb01051ed5ae
rutujakhodke <rutujakho6300@gmail.com>
2021-10-31 19:19:19 +0530

    Java Collection classes of AbstractList programs added

---
+++ Java_Programs/AbstractListEx1.java
@@ -0,0 +1,17 @@
+//1.Java Collection AbstractList example.
+import java.util.*;
+public class AbstractListEx1{
+ public static void main(String args[]){
+ //creating an empty AbstractList
+ AbstractList<String> abList=new ArrayList<String>();
+ //use add() method to adding elements in collection
+ abList.add("Welcome");
+ abList.add("To");
+ abList.add("Mkpits");
+ abList.add("Solutions");
+ abList.add("10");
+ abList.add("20");
+ //displaying elements in collection
+ System.out.println("AbstractList is: "+abList);
+ }
+}
