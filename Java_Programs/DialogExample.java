3730dd92cdbdcce348d00deac45b00ec7e391927
rutujakhodke <rutujakho6300@gmail.com>
2021-11-19 13:27:54 +0530

    Java Awt Dialogbox component program added

---
+++ Java_Programs/DialogExample.java
@@ -0,0 +1,25 @@
+//1.Java Awt Dialog example.
+import java.awt.*;
+import java.awt.event.*;
+public class DialogExample{
+ private static Dialog d;
+  DialogExample(){
+   Frame f=new Frame("Dialog Example");
+   d=new Dialog(f,"Dialog Example",true);
+   d.setLayout(new FlowLayout());
+
+   Button b=new Button("OK");
+   b.addActionListener(new ActionListener(){
+    public void actionPerformed(ActionEvent ae){
+     DialogExample.d.setVisible(false);
+    }
+   });
+   d.add(new Label("Click button to continue.."));
+   d.add(b);
+   d.setSize(300,300);
+   d.setVisible(true);
+  }
+    public static void main(String args[]){
+    DialogExample d=new DialogExample();
+}
+}
