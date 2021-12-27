a734d38f6fbbbdd7b25515d521738c72ff66f6da
rutujakhodke <rutujakho6300@gmail.com>
2021-12-27 19:54:21 +0530

    Deleted files

--- Java_Programs/SwingRegFormEx.java
+++ Java_Programs/SwingRegFormEx.java
@@ -2,7 +2,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class SwingRegFormEx extends Frame implements ActionListener{
+public class SwingRegFormEx extends JFrame implements ActionListener{
   JLabel l1,l2,l3,l4,l5,l6,l7,l8,l9;
   JTextField t1;
   JButton b1;
