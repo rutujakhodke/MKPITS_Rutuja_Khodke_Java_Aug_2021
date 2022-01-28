c49db7bffa8090e8ad76eac8d8c2b9105fd07ae5
rutujakhodke <rutujakho6300@gmail.com>
2022-01-28 17:36:10 +0530

    Servlet Config and context program added

---
+++ Adv_java_Programs/ServletExample3/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.example.servletex1;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println(getServletConfig().getServletName());//this will print servlet name
+        out.println(getServletConfig().getServletContext());//this will print context of servlet
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
