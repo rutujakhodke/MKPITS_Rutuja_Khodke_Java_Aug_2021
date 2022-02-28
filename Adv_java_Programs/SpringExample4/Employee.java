523cc6d3039e359498f1e6fc284763dcb1bcf87f
rutujakhodke <rutujakho6300@gmail.com>
2022-02-28 20:10:56 +0530

    Spring Constructor injection program added

---
+++ Adv_java_Programs/SpringExample4/Employee.java
@@ -0,0 +1,30 @@
+package com.example.springp4;
+
+public class Employee {
+    int id;
+    String name;
+
+    public Employee(int id) {
+        this.id = id;
+    }
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public Employee(int id,String name) {
+        this.id=id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    void display(){
+        System.out.println("hello " +getId()+" "+getName());
+    }
+}
