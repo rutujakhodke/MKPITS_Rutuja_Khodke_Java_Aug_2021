16ad14033d6e4a8c477f72de2de403ae4fc4e1e2
rutujakhodke <rutujakho6300@gmail.com>
2022-03-05 12:38:14 +0530

    Spring @Pointcut,JoinPoint and exception program of aop added

---
+++ Adv_java_Programs/SpringExample17/SpringP22Application.java
@@ -0,0 +1,22 @@
+package com.example.springp22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class SpringP22Application {
+
+    public static void main(String[] args) {
+        ApplicationContext appcont=new ClassPathXmlApplicationContext("bean.xml");
+        Employee eobj = appcont.getBean("emp",Employee.class);
+        System.out.println(eobj.getId());
+
+        Student sobj = appcont.getBean("stud",Student.class);
+        System.out.println(sobj.getId());
+        System.out.println(sobj.getName());
+        sobj.printThrowException();
+    }
+
+}
